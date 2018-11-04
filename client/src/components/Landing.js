cdf3726c3d4a3bdfc2c78b469bd23c1b9876948e
kevinchezh <31943110+kevinchezh@users.noreply.github.com>
2018-11-04 15:28:42 -0500

    V1 (#1)
    * fix a serious BUGGGGGGGGgit add .git add .
    
    * test if branch works

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,7 @@ class Landing extends Component {
 	render() {
 		return (
 			<div className = "container">
-				<h1>In the name of 车震 震震震! This will be our landing page</h1>
+				<h1>别以为我找不着这个！</h1>
 			</div>
 		);
 	}
